3f3dfadf8297d9573f131e53fcd2acd050ca2884
SidharthJ Dev <sid.j.dev@gmail.com>
2020-04-23 21:22:04 +0530

    Initial Commit

---
+++ SideMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideMenu
+//
+//  Created by Sidharth J Dev on 23/04/20.
+//  Copyright © 2020 Sidharth J Dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
